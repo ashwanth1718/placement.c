a02bef1dbe7acb79d8de94c88892eee959f3e880
ashwanth1718 <ashwanthnk652@gmail.com>
2025-05-16 15:01:37 +0530

    Add files via upload

---
+++ day 5/pattern.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+    int r =5;
+    for (int i = 0; i < r; i++) 
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
