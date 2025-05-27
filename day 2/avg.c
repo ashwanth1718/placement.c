8fe5f78f5d08d93584ad9e5578084cbdb67ab79d
ashwanth1718 <ashwanthnk652@gmail.com>
2025-05-27 09:44:57 +0530

    Update avg.c

--- day 2/avg.c
+++ day 2/avg.c
@@ -4,7 +4,7 @@ int main()
     int  a=50,b=55,c=65,d=40 , sum= a+b+c+d ,avg=sum/5;
     printf("the sum of value %d \n",sum);
     printf("the average %d \n",avg);
-    if(avg>89)
+    if(avg<89)
     {
         printf("A grade");
        }
